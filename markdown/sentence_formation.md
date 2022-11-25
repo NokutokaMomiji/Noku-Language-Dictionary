f089f98d1a8f95fce921d04f6006a8e13d4d003b
Emilia Peñaloza <58234390+NokutokaMomiji@users.noreply.github.com>
2022-11-25 17:34:32 +0100

    Data to JSON parsing pretty much done. Fix bugs with online and stuff.

---
+++ markdown/sentence_formation.md
@@ -0,0 +1,70 @@
+# **Verb Conjugation**
+
+**Verbs** in Noku are easy to conjugate. There are four types of verb conjugation in Noku: **Present** , **Past** , **Future** and **Imperative**. To demonstrate every conjugation type, we will be using the verb "To Be": "Mai"
+
+#
+## **Present Conjugation**
+The **Present** conjugation is used to indicate that an action is happening in the current moment. This conjugation merely utilizes the **verb root** and is the same as the **infinitive** form.
+
+**Ex:**
+
+I am -\> Ako **mai**.
+
+You are -\> Tako **mai.**
+
+He / She is -\> Mako **mai.**
+
+#
+## **Past Conjugation**
+
+The **Past** conjugation is used to indicate that an action happened previousto the current moment. This conjugation is done by appending the suffix " **-anon**" to the verb root.
+
+**Ex:**
+
+I was -\> Ako mai **anon.**
+
+You were -\> Tako mai **anon.**
+
+He / She was -\> Mako mai **anon.**
+
+#
+## **Future Conjugation**
+
+The **Future** conjugation is used to indicate that an action will happen in the, well, future. This conjugation is done by appending the suffix " **-rian**" to the verb root.
+
+**Ex:**
+
+I will be -\> Ako mai **rian.**
+
+You will be -\> Tako mai **rian.**
+
+He / She will be -\> Mako mai **rian.**
+
+## **Imperative Conjugation**
+
+The **Imperative** conjugation is used to order someone or give instructions to do something. This conjugation can only be used with **Action** verbs. This conjugation is done by simply repeating the first syllable, or first consonant and vowel in certain cases, before the root verb. Look at the following example using the verb "To Think": "Mikiro".
+
+**Ex:**
+
+Think, Gura! -\> **Mi** mikiro, Gura!
+
+Or the following example, giving an instruction using the verb "To press": "Neka"
+
+**Ex:**
+
+**Press** [Z] to interact. -\> **Neneka** [Z] interako.
+
+The imperative conjugation can only be used with either a noun, as done in the previous example, or with the 2nd Person Direct Pronoun (either singular or plural).
+
+## Participles and particles
+The Present conjugation is also used as the present participle of the word.
+
+**Ex:**
+
+I was **being** nice! -\> Ako maianon **mai** nindo!
+
+The Past conjugation is also used as the past particle of the word.
+
+**Ex:**
+
+I had **warned** you. -\> Ako milikianon amaranon kayo.
--- nlutil.py
+++ nlutil.py
@@ -1,16 +1,40 @@
 import json
+import gdown
+import pypandoc
+import os
+import socket
+import traceback
+
+def create_nonexistent_directory(directory_name: str) -> None:
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
 
 def open_file_to_list(file_path: str) -> list:
+    # Currently using utf-8.
     with open(file_path, "r", encoding="utf-8") as data_file:
         return data_file.readlines()
 
+def load_saved_data():
+    with open("build/data.json") as stored_data_file:
+        return json.load(stored_data_file)
+
 def array_remove_empty_lines(array: list) -> list:
+    final_array = []
     for index in range(len(array)):
         line = array[index]
-        line = str.replace("\n", "")
-        if len(''.join(char for char in line if line.isalnum())) == 0:
-            array.pop(index)
-    return array
+
+        # Remove new lines and replace the weird quotation marks with the standard ones.
+        line = line.replace("\n", "")
+        line = line.replace("“", "\"")
+        line = line.replace("”", "\"")
+
+        # Check if string is empty by removing special characters.
+        if len(''.join(char for char in line if char not in ["\n", "_"]).replace("\n", "")) == 0:
+           continue
+        
+        # Line is clear, so we add it to our final array.
+        final_array.append(line) 
+    return final_array
 
 def word_info_to_dict(info_line: str) -> dict:
     # Words and their definitions are stored in the following format:
@@ -27,8 +51,9 @@ def word_info_to_dict(info_line: str) -> dict:
     in_word = True
     in_info = False
 
+    # Reading word and type.
     for char in line_contents[0]:
-        if char != "(" and in_word:
+        if char != "(" and in_word and char != " ":
             word += char
             continue
         
@@ -44,6 +69,7 @@ def word_info_to_dict(info_line: str) -> dict:
         if char == ")" and in_info:
             in_info = False
 
+    # Reading definitions and adding them to a definitions array.
     for char in line_contents[1]:
         if char == " " and not in_quotations:
             continue
@@ -52,88 +78,204 @@ def word_info_to_dict(info_line: str) -> dict:
             in_quotations = not in_quotations
             continue
 
-        if char == "," and in_quotations:
+        if (char == "," and in_quotations) or char != ",":
             current_definition += char
             continue
         
         if char == "," and not in_quotations:
             word_definitions.append(current_definition)
+            print(f"Current Definition: {current_definition}")
             current_definition = ""
 
-    return {"word": word, "type": word_type, "definitions": word_definitions}
-    
-        
+    # Appending residue.
+    word_definitions.append(current_definition)
+    print(f"Current Definition: {current_definition}")
 
-def parse_words_to_json() -> dict:
-    print("Running...")
+    print(f"Word: \"{word}\"")
+    print(f"Type: \"{word_type}\"")
+    print(f"Definitions: \"{word_definitions}\"")
+    return {"word": word, "type": word_type, "definitions": word_definitions}
+
+def can_advance_index(index: int, limit: int) -> bool:
+    return ((index + 1) < limit)
+
+def parse_words_to_json(file_path: str) -> dict:
+    print("[JSON Parser]: Parsing to JSON...")
     SEPARATOR = "________________"
 
-    content = open_file_to_list("src/noku_language_words.txt")
-    
+    content = open_file_to_list(file_path)
+    content = array_remove_empty_lines(content)
+
     print(content)
-    array_remove_empty_lines(content)
-
     data_dict = {}
-    current_letter = "A"
+    current_letter = ""
+    content_length = len(content)
+    index = 0
 
-    for index in range(len(content)):
+    print(f"[JSON Parser]: There are {content_length} lines.")
+
+    while index < content_length:
         line = content[index]
-        print("line: ", line)
-        # Separators indicate the beginning of a new section of words.
-        if line == SEPARATOR:
+        print(f"[JSON Parser]: Current index: {index}")
+        print(f"[JSON Parser]: Current line: {line}")
+        print(f"[JSON Parser]: Current line length: {len(line)}")
+        print("[JSON Parser]: Is letter: ", line in ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"])
+        # Probably a letter, which indicates a new section.
+        if len(line) == 1:
+            print("[JSON Parser]: Line is separator.")
+            current_letter = line
+            print(f"[JSON Parser]: New letter '{current_letter}'")
+            if not can_advance_index(index, content_length):
+                break
             index += 1
-            current_letter = content[index]
-            index += 1
+            print("[JSON Parser]: Creating new dictionary.")
+            data_dict[current_letter] = {}
             data_dict[current_letter]["description"] = content[index]
             data_dict[current_letter]["words"] = []
+            temp = data_dict[current_letter]["description"]
+            print(f"[JSON Parser]: Letter Description: \"{temp}\".")
+            index += 1
             continue
         
+        if current_letter == "":
+            index += 1
+            continue
+
         # Examples are automatically extracted after finding the definition for a word. 
         # So, if we find ourselves at an example, it is orphan and an error.
         # So, we report it.
         if line[:2].lower() == "ex":
             print(f"[ERROR]: Orphan example \"{line}\".")
+            index += 1
             continue
 
         word_info = word_info_to_dict(line)
         word_info["examples"] = []
-
+        if not can_advance_index(index, content_length):
+            break
         index += 1
         line = content[index]
+        line_length = len(line)
 
-        while line[:2].lower() == "ex":
+        while line[:3].lower() == "ex:":
             in_english = True
             skip_spaces = True
             current_example = ""
+            subindex = 4
 
-            for subindex in range(3, len(line)):
+            while subindex < line_length:
                 char = line[subindex]
+                next_char = ""
+                if can_advance_index(subindex, line_length):
+                    next_char = line[subindex + 1]
+
                 if char == " " and skip_spaces:
-                    continue
-                
-                if char == "-" and line[subindex + 1] == ">":
-                    word_info["examples"].append(current_example)
-                    current_example = ""
+                    if not can_advance_index(subindex, line_length):
+                        break
                     subindex += 1
                     continue
+                
+                if char == "-" and next_char == ">":
+                    word_info["examples"].append(current_example)
+                    print(f"current_example: {current_example}")
+                    current_example = ""
+                    if not can_advance_index(subindex, line_length):
+                        break
+                    subindex += 3
+                    skip_spaces = True
+                    continue
 
-                if char != " " or (char == " " and not skip_spaces):
+                if char != " " or (char == " " and not skip_spaces and next_char != "-"):
                     skip_spaces = False
-                    current_example += ""
+                    current_example += char
 
+                if not can_advance_index(subindex, line_length):
+                    break
+
+                subindex += 1
+            print(f"current_example: \"{current_example}\"")
             word_info["examples"].append(current_example)
+            if not can_advance_index(index, content_length):
+                break
+            index += 1
+            line = content[index]
+            line_length = len(line)
+
+        print(f"Remaining line: {line}")
+        print(f"Removing line.")
+        index -= 1
 
         data_dict[current_letter]["words"].append(word_info)
+        if not can_advance_index(index, content_length):
+            break
+        index += 1
 
     return data_dict
-        
+  
 
 if __name__ == "__main__":
     print("======[Noku Language]======")
     print(" · For official document, check the following link: ")
     print("   -> https://docs.google.com/document/d/1o37wxGVKjD7Y0m-wSIY2T8IX4aHma1fx/edit?usp=sharing&ouid=113385481694575437883&rtpof=true&sd=true")
+    
+    create_nonexistent_directory("build")
+    create_nonexistent_directory("src")
+    create_nonexistent_directory("build/html")
 
-    exported_data = parse_words_to_json()
-#as
-    with open("data.json", "w") as export_data:
+    URL = "https://drive.google.com/uc?id=1o37wxGVKjD7Y0m-wSIY2T8IX4aHma1fx"
+    
+    TEMP_NAME = "src/temp.docx"
+    OUTPUT_NAME = "src/noku_language_words.txt"
+    DATA_NAME = "build/data.json"
+    BACKUP_NAME = "src/backup.txt"
+
+    exported_data = {}
+
+    try:
+        print("[Info]: Downloading online dictionary.")
+        gdown.download(URL, TEMP_NAME, quiet=False)
+
+        pypandoc.ensure_pandoc_installed()
+        pypandoc.convert_file(TEMP_NAME, "plain", outputfile=OUTPUT_NAME)
+    except socket.gaierror:
+        print("[ERROR]: (11001) Failed to get address info.")
+        with open("traceback.txt", "w") as tb_file:
+            tb_file.write(traceback.format_exc())
+
+        print("[ERROR]: Traceback exported.")
+    except:
+        print("[ERROR]: An error ocurred while downloading the data from the online version.")
+        with open("traceback.txt", "w") as tb_file:
+            tb_file.write(traceback.format_exc())
+
+        print("[ERROR]: Traceback exported.")
+    finally:
+        if os.path.exists(DATA_NAME):
+            print("[Info]: Data file detected. Loading...")
+            exported_data = load_saved_data()
+        elif os.path.exists(BACKUP_NAME):
+            print("[Info]: Backup file detected. Parsing...")
+            exported_data = parse_words_to_json(BACKUP_NAME)
+        else:
+            print("[ERROR]: Data file not found. Exiting...")
+            exit(1)
+   
+
+    try:
+        if len(exported_data) == 0:
+            exported_data = parse_words_to_json(OUTPUT_NAME)
+    except:
+        print("[ERROR]: An error ocurred while parsing the data from the online version.")
+        if os.path.exists(DATA_NAME):
+            print("[Info]: Data file detected. Loading...")
+            exported_data = load_saved_data()
+        elif os.path.exists(BACKUP_NAME):
+            print("[Info]: Backup file detected. Parsing...")
+            exported_data = parse_words_to_json(BACKUP_NAME)
+        else:
+            print("[ERROR]: Data file not found. Exiting...")
+            exit(1)
+    
+
+    with open(DATA_NAME, "w") as export_data:
         json.dump(exported_data, export_data)
