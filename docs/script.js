874badca5d7ae798aae86e6bb1e443563b53d8bf
Emilia Peñaloza <58234390+NokutokaMomiji@users.noreply.github.com>
2022-11-28 16:38:18 +0100

    WORDS!

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,35 @@
 var dictionaryData = {};
 
 fetch("data/data.json").then(response => response.json())
-                       .then(data => dictionaryData = data)
+                       .then(data => Setup(data))
                        .catch(error => console.error(error));
 
 console.log(dictionaryData);
+
+function Setup(data) {
+    dictionaryData = data;
+    setLetterList();
+    setWordList();
+    setWordPage();
+}
 
 function getLetters() {
     return Object.keys(dictionaryData);
 }
 
 function setLetterList() {
+    console.log(dictionaryData);
+    if (dictionaryData.length == 0) {
+        return setLetterList();
+    }
+
     var letters = getLetters();
     var letterList = document.getElementById("letter-list");
+
+    if (letterList == undefined) {
+        return;
+    }
+
     for (var i = 0; i < letters.length; i++) {
         var newItem = document.createElement("li");
         var newItemLink = document.createElement("a");
@@ -31,13 +48,18 @@ function setWordList() {
     var letter = new URLSearchParams(window.location.search).get("letter");
     if (letter == null) {
         console.error("Letter returned null.");
-        return;
+        return setWordList();
     }
 
     var words = dictionaryData[letter]["words"];
     var wordTitle = document.getElementById("current-letter");
     var wordDescription = document.getElementById("letter-description");
     var wordList = document.getElementById("word-list");
+
+    if (wordTitle == undefined || wordDescription == undefined || wordList == undefined) {
+        return;
+    }
+
     document.title = "Noku Language - " + letter;
     wordTitle.innerText = letter;
     wordDescription.innerText = dictionaryData[letter]["description"];
@@ -60,4 +82,60 @@ function setWordPage() {
     var word = new URLSearchParams(window.location.search).get("word");
     var words = dictionaryData[letter]["words"];
     var wordData = undefined;
+
+    var wordTitle = document.getElementById("word-title");
+    var wordType = document.getElementById("word-type");
+    var possibleVerbForms = document.getElementById("possible-verb-forms");
+    var wordDefinitions = document.getElementById("word-definitions");
+    var wordExamples = document.getElementById("word-examples");
+    var contentBefore = document.getElementById("before-text");
+    var contentAfter = document.getElementById("after-text");
+
+    for (var i = 0; i < words.length; i++) {
+        if (words[i]["word"] == word) {
+            wordData = words[i];
+            wordTitle.textContent = word;
+            wordType.textContent = wordData["type"];
+            if (wordData["type"].includes("Verb")) {
+                possibleVerbForms.innerHTML = "Past: <span>" + word + "anon" + "</span> | Future: <span>" + word + "rian" + "</span> | Imperative: <span>" + word.slice(0, 2) + word.toLowerCase() + "</span>"
+            }
+            for (var n = 0; n < wordData["definitions"].length; n++) {
+                var newDefinition = document.createElement("li");
+                newDefinition.textContent = "\"" + wordData["definitions"][n] + "\"";
+                wordDefinitions.appendChild(newDefinition);
+            }
+
+            for (var n = 0; n < wordData["examples"].length; n++) {
+                var newExample = document.createElement("li");
+                var newBreak = document.createElement("br");
+                var newTranslated = document.createElement("span");
+                newTranslated.textContent = wordData["examples"][n];
+                if (n + 1 < wordData["examples"].length) {
+                    newExample.textContent = wordData["examples"][++n];
+                }
+
+                newExample.appendChild(newBreak)
+                newExample.appendChild(newTranslated);
+                wordExamples.appendChild(newExample);
+            }
+
+            if (i != 0) {
+                contentBefore.textContent = words[i - 1]["word"];
+                contentBefore.href = "word.html?letter=" + letter + "&word=" + words[i - 1]["word"];
+            }
+            else {
+                contentBefore.remove();
+            }
+            
+            if (i != words.length - 1) {
+                contentAfter.textContent = words[i + 1]["word"];
+                contentAfter.href = "word.html?letter=" + letter + "&word=" + words[i + 1]["word"];
+            }
+            else {
+                contentAfter.remove();
+            }
+
+            break;
+        }
+    }
 }
---
+++ markdown/sentence_formation.md
@@ -0,0 +1,37 @@
+# Sentence Formation
+
+## Standard Sentences
+
+In most cases, sentences in Noku follow a similar structure to English sentences.
+
+**Ex:**
+
+> I want coffee. -\> Ako nalo fosi.
+
+However, sentences in Noku are bound by certain rules depending on the type of sentence.
+
+Normal sentences always follow a Subject-Verb-Object structure:
+
+**Ex:**
+
+> I am a nice person. -\> Ako mai ang nindo imaniti.
+> 
+> Well, I love you. -\> Owu, ako ame kayo.
+> 
+> Owu ako ame kayo -\> THIS IS **INCORRECT** (X).
+
+## Questions
+
+Questions follow a Question Word / Conditional (if any) - Verb - Subject - Object structure:
+
+**Ex:**
+
+> How are you doing? -\> Gama mai tako lakan?
+
+Keep in mind that this applies to all questions. So when translating from English to Noku, you may have to switch things around or add things.
+
+**Ex:**
+
+You did that? -\> Lakananon tako eto?
+
+Did what? -\> Apo lakananon ako?
